2845543eff478706103f4c8733e772e66cba0fe3
K1nGsmaN-hub <ilya@avex.kz>
2021-04-27 21:19:29 +0800

    add custom _document in pages/

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import GlobalStyle from "app/sc/GlobalStyle";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <>
+                <App {...props} />
+                <GlobalStyle />
+              </>
+            ),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
+
+export default MyDocument;
